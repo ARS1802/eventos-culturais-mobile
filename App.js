c204aef9d7f727db72c511d244d36bf2e8a02ebc
ARS1802 <arsouza1802@gmail.com>
2026-04-07 01:36:06 -0300

    Initial Commit.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+//no terminal, ponha: npx expo start
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={StyleSheet.create({color: '#d1a38f'})}>OMEGA TEXT!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4ebdd',
     alignItems: 'center',
     justifyContent: 'center',
   },
