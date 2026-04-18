d557acee6bc1f01fa84de063a35956534d63bb16
João Pedro Carvalho <carvalhojp2410@gmail.com>
2026-04-18 18:03:28 -0300

    Commit

--- App.js
+++ App.js
@@ -1,23 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MainContainer from './components/MainContainer.jsx';
-import Input from './components/TextInput.jsx';
-//no terminal, ponha: npx expo start
-export default function App() {
-  const [nome, setNome] = useState('');
-  return (
-      <MainContainer 
-      bottom = {<Text>"botom = embaixo"</Text>} 
-      top = {<Text>"top = emcima"</Text>}
-      >
-        <TextInput placeholder="Digite algo..." />
+import React from "react";
+import AppNavigator from "./navigation/AppNavigation";
 
-        <Input
-        placeholder="Digite seu nome"
-        value={nome}
-        onChangeText={setNome}
-      />
-      </MainContainer>
-      
-  );
+export default function App() {
+  return <AppNavigator />;
 }
---
+++ navigation/AppNavigation.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text } from "react-native";
+import MainContainer from "../components/MainContainer";
+import Header from "../components/Header";
+import Bottom from "../components/Bottom";
+
+const Stack = createNativeStackNavigator();
+
+const LoginScreen = () => (
+  <MainContainer
+    top={<Header title="Espaços Culturais" />}
+    bottom={<Bottom tipo="visitante" onFiltro={() => alert("filtro!")} />}
+  >
+    <Text>Tela de Login</Text>
+  </MainContainer>
+);
+
+const TelaTemp = ({ nome }) => <Text>{nome}</Text>;
+const CadastroScreen = () => <TelaTemp nome="Cadastro" />;
+const FeedVisitanteScreen = () => <TelaTemp nome="Feed Visitante" />;
+const FeedOrganizadorScreen = () => <TelaTemp nome="Feed Organizador" />;
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Cadastro" component={CadastroScreen} />
+        <Stack.Screen name="FeedVisitante" component={FeedVisitanteScreen} />
+        <Stack.Screen name="FeedOrganizador" component={FeedOrganizadorScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
